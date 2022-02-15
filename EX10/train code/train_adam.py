e9254151cbdc45a6ac4b4503069df77ad65118b8
ChaeeunShin <96675556+chaen805@users.noreply.github.com>
2022-02-16 00:12:28 +0900

    Add files via upload

---
+++ EX10/train code/train_adam.py
@@ -0,0 +1,259 @@
+import os
+import glob
+import time
+
+import PIL
+import imageio
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
+from IPython import display
+import matplotlib.pyplot as plt
+
+from matplotlib.pylab import rcParams
+
+
+
+cifar10 = tf.keras.datasets.cifar10
+
+(train_x, _), (test_x, _) = cifar10.load_data()
+
+print("데이터 로드 완료!")
+
+train_x = train_x / 127.5 - 1# 이미지를 [-1, 1]로 정규화합니다.
+
+BUFFER_SIZE = 50000
+BATCH_SIZE = 256
+
+train_dataset = tf.data.Dataset.from_tensor_slices(train_x).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+
+# generator
+def make_generator_model():
+
+    # Start
+    model = tf.keras.Sequential()
+
+    # First: Dense layer
+    model.add(layers.Dense(8*8*256, use_bias=False, input_shape=(100,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    # Second: Reshape layer
+    model.add(layers.Reshape((8, 8, 256)))
+
+    # Third: Conv2DTranspose layer
+    model.add(layers.Conv2DTranspose(128, kernel_size=(5, 5), strides=(1, 1), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    # Fourth: Conv2DTranspose layer
+    model.add(layers.Conv2DTranspose(64, kernel_size=(5, 5), strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    # Fifth: Conv2DTranspose layer
+    model.add(layers.Conv2DTranspose(3, kernel_size=(5, 5), strides=(2, 2), padding='same', use_bias=False, \
+                                     activation='tanh'))
+
+    return model
+
+generator = make_generator_model()
+
+generator.summary()
+
+
+# discriminator
+def make_discriminator_model():
+
+    # Start
+    model = tf.keras.Sequential()
+
+    # First: Conv2D Layer
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same', input_shape=[32, 32, 3]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    # Second: Conv2D Layer
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    # Third: Flatten Layer
+    model.add(layers.Flatten())
+
+    # Fourth: Dense Layer
+    model.add(layers.Dense(1))
+
+    return model
+
+discriminator = make_discriminator_model()
+
+discriminator.summary()
+
+
+# 학습 관련 세팅
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+def discriminator_accuracy(real_output, fake_output):
+    real_accuracy = tf.reduce_mean(tf.cast(tf.math.greater_equal(real_output, tf.constant([0.5])), tf.float32))
+    fake_accuracy = tf.reduce_mean(tf.cast(tf.math.less(fake_output, tf.constant([0.5])), tf.float32))
+    return real_accuracy, fake_accuracy
+
+generator_optimizer = tf.keras.optimizers.Adam(2e-4, 0.5)
+discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, 0.5)
+
+noise_dim = 100
+num_examples_to_generate = 16
+
+seed = tf.random.normal([num_examples_to_generate, noise_dim])
+
+
+@tf.function
+def train_step(images):  #(1) 입력데이터
+    noise = tf.random.normal([BATCH_SIZE, noise_dim])  #(2) 생성자 입력 노이즈
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:  #(3) tf.GradientTape() 오픈
+        generated_images = generator(noise, training=True)  #(4) generated_images 생성
+
+        #(5) discriminator 판별
+        real_output = discriminator(images, training=True)
+        fake_output = discriminator(generated_images, training=True)
+
+        #(6) loss 계산
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+        #(7) accuracy 계산
+        real_accuracy, fake_accuracy = discriminator_accuracy(real_output, fake_output) 
+    
+    #(8) gradient 계산
+    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    #(9) 모델 학습
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+    return gen_loss, disc_loss, real_accuracy, fake_accuracy  #(10) 리턴값
+
+
+def generate_and_save_images(model, epoch, it, sample_seeds):
+
+    predictions = model(sample_seeds, training=False)
+    
+    np_predictions = predictions.numpy()
+    np_predictions = (np_predictions + 1) * 127.5 # 정규화 했던 것을 되돌리기
+    np_predictions = np_predictions.astype(int)
+
+    fig = plt.figure(figsize=(4, 4))
+    for i in range(np_predictions.shape[0]):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(np_predictions[i])
+        plt.axis('off')
+   
+    plt.savefig('./generated_samples/sample_epoch_{:04d}_iter_{:03d}.png'.format(epoch, it))
+    plt.close(fig)
+
+
+def draw_train_history(history, epoch):
+    fig = plt.figure(figsize=(15, 15))
+    # summarize history for loss  
+    plt.subplot(211)  
+    plt.plot(history['gen_loss'])  
+    plt.plot(history['disc_loss'])  
+    plt.title('model loss')  
+    plt.ylabel('loss')  
+    plt.xlabel('batch iters')  
+    plt.legend(['gen_loss', 'disc_loss'], loc='upper left')  
+
+    # summarize history for accuracy  
+    plt.subplot(212)  
+    plt.plot(history['real_accuracy'])  
+    plt.plot(history['fake_accuracy'])  
+    plt.title('discriminator accuracy')  
+    plt.ylabel('accuracy')  
+    plt.xlabel('batch iters')  
+    plt.legend(['real_accuracy', 'fake_accuracy'], loc='upper left')  
+    
+    # training_history 디렉토리에 epoch별로 그래프를 이미지 파일로 저장합니다.
+    plt.savefig('./training_history/train_history_{:04d}.png'.format(epoch))
+    plt.close(fig)
+
+
+checkpoint_dir = './training_checkpoints'
+
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
+
+
+def train(dataset, epochs, save_every):
+    start = time.time()
+    history = {'gen_loss':[], 'disc_loss':[], 'real_accuracy':[], 'fake_accuracy':[]}
+
+    for epoch in range(epochs):
+        epoch_start = time.time()
+        for it, image_batch in enumerate(dataset):
+            gen_loss, disc_loss, real_accuracy, fake_accuracy = train_step(image_batch)
+            history['gen_loss'].append(gen_loss)
+            history['disc_loss'].append(disc_loss)
+            history['real_accuracy'].append(real_accuracy)
+            history['fake_accuracy'].append(fake_accuracy)
+
+            if it % 50 == 0:
+                display.clear_output(wait=True)
+                generate_and_save_images(generator, epoch+1, it+1, seed)
+                print('Epoch {} | iter {}'.format(epoch+1, it+1))
+                print('Time for epoch {} : {} sec'.format(epoch+1, int(time.time()-epoch_start)))
+
+        if (epoch + 1) % save_every == 0:
+            checkpoint.save(file_prefix=checkpoint_prefix)
+
+        display.clear_output(wait=True)
+        generate_and_save_images(generator, epochs, it, seed)
+        print('Time for training : {} sec'.format(int(time.time()-start)))
+
+        draw_train_history(history, epoch)
+
+
+save_every = 5
+EPOCHS = 700
+
+# 사용가능한 GPU 디바이스 확인
+tf.config.list_physical_devices("GPU")
+
+print("학습시작")
+
+train(train_dataset, EPOCHS, save_every)
+
+# gif 만들기
+anim_file = './cifar_dcgan_ver1.gif'
+
+with imageio.get_writer(anim_file, mode='I') as writer:
+    filenames = glob.glob('./generated_samples/sample*.png')
+    filenames = sorted(filenames)
+    last = -1
+    for i, filename in enumerate(filenames):
+        frame = 2*(i**0.5)
+        if round(frame) > round(last):
+            last = frame
+        else:
+            continue
+        image = imageio.imread(filename)
+        writer.append_data(image)
+    image = imageio.imread(filename)
+    writer.append_data(image)
+
+print("gif 생성 완료!")
+print("학습 종료")
